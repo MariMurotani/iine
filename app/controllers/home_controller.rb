23e95db2ac409bcf3113a0e4093c68746d56f6a9
MariMurotaniENE <mari.murotani@enechange.co.jp>
2019-03-28 17:28:30 +0900

    Update sound

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,12 @@ class HomeController < ApplicationController
     counter.save!
     redirect_to '/'
   end
+  def special1
+    system("open /Users/mari.murotani/Source/iine/app/assets/line-girl1_line-girl1-oo2.mp3")
+    redirect_to '/'
+  end
+  def special2
+    system("open /Users/mari.murotani/Source/iine/app/assets/line-girl1_line-girl1-ee2.mp3")
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'reset', to: 'home#reset'
+  get 'special1', to: 'home#special1'
+  get 'special2', to: 'home#special2'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'application#home'
 end
