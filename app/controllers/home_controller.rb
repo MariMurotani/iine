26b4af32f24d167798b552135afcd5e025b758b3
MariMurotaniENE <mari.murotani@enechange.co.jp>
2019-03-28 17:31:00 +0900

    Update sound

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,16 @@ class HomeController < ApplicationController
     redirect_to '/'
   end
   def special1
+    counter = Counter.new(count: 0)
+    counter.count = counter.count + 10
+    counter.save!
     system("open /Users/mari.murotani/Source/iine/app/assets/line-girl1_line-girl1-oo2.mp3")
     redirect_to '/'
   end
   def special2
+    counter = Counter.new(count: 0)
+    counter.count = counter.count + 20
+    counter.save!
     system("open /Users/mari.murotani/Source/iine/app/assets/line-girl1_line-girl1-ee2.mp3")
     redirect_to '/'
   end
